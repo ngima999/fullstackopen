a4ae9114b89a069113b1748b810316f63d846265
Ngima Tashi Sherpa <np03cs4s220025@heraldcollege.edu.np>
2024-12-14 23:52:13 +0545

    Ex. 4.4 (Step 2) : Implement totalLikes function and write unit tests for blog list

--- part_4/BlogList/test/utils.test.js
+++ part_4/BlogList/test/utils.test.js
@@ -1,10 +1,27 @@
-const { test } = require('node:test')
+const { test, describe } = require('node:test')
 const assert = require('node:assert')
-const listHelper = require('../utils/list_helper')
+const { dummy, totalLikes, favoriteBlog } = require('../utils/list_helper')
 
-test('dummy returns one', () => {
-  const blogs = []  // Passing an empty array, but the function should always return 1
+const blogs = [
+  { title: 'Blog 1', author: 'Author 1', likes: 5 },
+  { title: 'Blog 2', author: 'Author 2', likes: 10 },
+  { title: 'Blog 3', author: 'Author 3', likes: 7 }
+]
 
-  const result = listHelper.dummy(blogs)
-  assert.strictEqual(result, 1)  // Verify that the function returns 1
+describe('dummy', () => {
+  test('returns 1', () => {
+    assert.strictEqual(dummy(blogs), 1)
+  })
 })
+
+describe('totalLikes', () => {
+  test('of an empty list is zero', () => {
+    assert.strictEqual(totalLikes([]), 0)
+  })
+
+  test('of a list with blogs is calculated correctly', () => {
+    assert.strictEqual(totalLikes(blogs), 22)
+  })
+})
+
+
