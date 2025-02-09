e6580b63b87897a6eb46c3831c42c583c7ef459c
Ngima Tashi Sherpa <np03cs4s220025@heraldcollege.edu.np>
2025-02-10 00:17:11 +0545

    E.X 6.10 ( Step 8 ): Refactor filterReducer to use createSlice from Redux Toolkit

--- Part_6/redux-anecdotes/src/App.jsx
+++ Part_6/redux-anecdotes/src/App.jsx
@@ -3,6 +3,7 @@ import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 const App = () => {
 
+  
   return (
     <div>
       <h2>Anecdotes</h2>
--- Part_6/redux-anecdotes/src/main.jsx
+++ Part_6/redux-anecdotes/src/main.jsx
@@ -1,7 +1,19 @@
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit' // Import configureStore from the correct path
 import store from './store'  // Import the store from the correct path
+import anecdoteReducer from './reducers/anecdoteReducer'
 import App from './App'
+
+
+const store = configureStore({
+  reducer: {
+    anecdotes: anecdoteReducer,
+    filter: filterReducer
+  }
+})
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
