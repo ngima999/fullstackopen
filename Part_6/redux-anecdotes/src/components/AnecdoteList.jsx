d1f81cedb6189e7ee44481df18b98f01e80ba39d
Ngima Tashi Sherpa <np03cs4s220025@heraldcollege.edu.np>
2025-02-09 23:41:22 +0545

    E.X  6.9 ( Step 1 ): Implement filtering for anecdotes using Redux state

--- Part_6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ Part_6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,30 +1,37 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { voteAnecdote } from '../reducers/anecdoteReducer'
-function AnecdoteList() {
-    const anecdotes = useSelector(state => state)
 
-    // sort by votes
-    const sortedAnecdotes = anecdotes.sort((a, b) => b.votes - a.votes)
-    const dispatch = useDispatch()
-  
-    const vote = (id) => {
-      dispatch( voteAnecdote(id) )
-      console.log('vote', id)
-    }
+function AnecdoteList() {
+  const anecdotes = useSelector(state => state.anecdotes) // directly access anecdotes array
+  const filter = useSelector(state => state.filter) 
+  const dispatch = useDispatch()
+
+
+   // Apply the filter first, then sort the anecdotes by votes
+   const filteredAnecdotes = anecdotes
+   .filter(anecdote =>
+     anecdote.content.toLowerCase().includes(filter.toLowerCase()) // Filter case-insensitively
+   )
+   .sort((a, b) => b.votes - a.votes) // Sort after filtering by votes
+
+
+  const vote = (id) => {
+    dispatch(voteAnecdote(id))
+    console.log('vote', id)
+  }
 
   return (
     <div>
-        {sortedAnecdotes.map(anecdote =>
+      {filteredAnecdotes.map(anecdote => (
         <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
+          <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
             <button onClick={() => vote(anecdote.id)}>vote</button>
           </div>
         </div>
-      )}</div>
+      ))}
+    </div>
   )
 }
 
